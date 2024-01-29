f16d62c283f753602f430d99f66d1b1bfad4e6b6
danielecarpentiero <carpentierodaniele@gmail.com>
2024-01-29 16:19:43 +0100

    feat: scaffolding

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import path from path;
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/scss/app.scss',
-                'resources/js/app.js'
-            ],
-            refresh: true
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
+            refresh: true,
         }),
     ],
     resolve: {
         alias: {
-            '~resources': '/resources/',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
+            "~resources": "/resources/",
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        },
     },
 });
